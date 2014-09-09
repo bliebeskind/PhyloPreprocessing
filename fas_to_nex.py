541aeef230093908f6ba5927be96d9a78449f0e8
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2014-09-09 14:18:47 -0500

    Added executable functionality to fas_to_nex.
    Put in a if _name_ clause at the bottom.

--- fas_to_nex.py
+++ fas_to_nex.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import sys
 from Bio import AlignIO
 from Bio.Alphabet import IUPAC, Gapped
 
@@ -10,3 +11,14 @@ def fas_to_nex(infile,outfile,protein=True):
 	else:
 		aln = AlignIO.read(infile,'fasta',alphabet=Gapped(IUPAC.unambiguous_dna))
 	AlignIO.write(aln,outfile,'nexus')
+	
+if __name__ == '__main__':
+	Usage = """
+	#### Usage ####
+	fas_to_nex.py <infile> <outfile>
+	"""
+	try:
+		infile,outfile = sys.argv[1], sys.argv[2]
+		fas_to_nex(infile,outfile)
+	except IndexError:
+		print Usage
